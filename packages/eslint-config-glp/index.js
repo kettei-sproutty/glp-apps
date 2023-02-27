feac4f5feb4c40bdd786cae25179718d1014e029
alessio.marchi <alessio.marchi@accenture.com>
2023-02-27 19:42:52 +0100

    fix(config): remove next/babel

--- packages/eslint-config-glp/index.js
+++ packages/eslint-config-glp/index.js
@@ -11,9 +11,4 @@ module.exports = {
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('next/babel')],
-    },
-  },
 }
